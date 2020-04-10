f15f8883b326ae08924f71b87d87d8d636684156
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-04-10 16:07:30 +0200

    moved venv out of the folder, since it didnt seem to work here

--- music/Scripts/activate.bat
+++ music/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\caspa\Documents\Code\Python Projects\Music\music
+set VIRTUAL_ENV=C:\Users\caspa\Documents\Code\Python Projects\Music\Partitur\music
 
 if not defined PROMPT set PROMPT=$P$G
 
