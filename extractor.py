6a7313a42448bf515ec61363db90e04cc4062a89
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-04-11 08:20:46 +0200

    rename extractor, upload file to test

---
+++ extractor.py
@@ -0,0 +1,15 @@
+from scipy.io import wavfile
+
+class Extractor:
+    """class used to extract a numpy array from a .wav file
+    input:
+        path to .wav
+    output:
+        numpy array (extract method)
+    """
+    def __init__(self, file):
+        self.file = file
+    def extract(self):
+        pass #TODO https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.read.html https://stackoverflow.com/questions/2060628/reading-wav-files-in-python
+        fs, data = wavfile.read('./output/audio.wav')
+        return fs, data
