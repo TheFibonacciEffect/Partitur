0ef45ea2ffc6168a1cfa5eece503b3641911386f
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-04-13 11:44:16 +0200

    extended commentry, implement extraction beginning, end

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
-from main import Extractor
+from main import Extractor, Transformator
 
-extract = Extractor(r"528hz Pure tone No Music-IMeusltcDGM.wav")
+extract = Extractor(r"60 Hz Test Tone-GqwFimG3X3w.wav")
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-print(extract.extract().__len__())
 extract.plot(sample_beginning= 4.2,sample_end=4.5)
+
+Transformator(r"60 Hz Test Tone-GqwFimG3X3w.wav").plot()
--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import numpy as np
 
 class Extractor:
     """class used to extract a numpy array from a .wav file
+    
     input:
         path to .wav
+    
     methods:
         extract, returns the data as a np array
         plot(), plots it uning the matplotlib    
@@ -17,10 +19,12 @@ class Extractor:
         fs, data = wavfile.read(self.file) #extract data
         self.samplesPerSecond = fs
         self.data = data    #type = matrix (sort of)
-    def extract(self, channel = 0):
-        """ input: channel (mostly 1 or 0)
+    def extract(self, channel = 0, beginning=0, end = -1):
+        """ input: channel (mostly 1 or 0), beginning (seconds), end (seconds)
+
         output: the data of the channel using a numphy array """
-        return self.data[:,channel]
+        beginning, end = int(beginning * self.samplesPerSecond), int(end * self.samplesPerSecond)
+        return self.data[:,channel][beginning:end] #this is not yet in seconds, see the plot method on how to implement
     def plot(self, channel = 0,sample_end = None, sample_beginning = 0): #in seconds
         """ input: channel (std = 0), sample_end in seconds, sample_beginning in seconds
         plots the music using matplotlib, the self.extract() method will have to be called first """
@@ -45,9 +49,33 @@ class Transformator(Extractor):
         """gets values and fourrier transforms them"""
         super().__init__(file)
         #TODO do fourrier transform
+        #how does the x axis need to look like in oder to give the right scale and make frequencies readable?
 
         # fft(x, n=None, axis=-1, norm=None, overwrite_x=False, workers=None)
         # Compute the one-dimensional discrete Fourier Transform.
+        # Parameters
+        # ----------
+        # x : array_like
+        #     Input array, can be complex.
+        # n : int, optional
+        #     Length of the transformed axis of the output.
+        #     If `n` is smaller than the length of the input, the input is cropped.
+        #     If it is larger, the input is padded with zeros.  If `n` is not given,
+        #     the length of the input along the axis specified by `axis` is used.
+        # axis : int, optional
+        #     Axis over which to compute the FFT.  If not given, the last axis is
+        #     used.
+        # norm : {None, "ortho"}, optional
+        #     Normalization mode. Default is None, meaning no normalization on the
+        #     forward transforms and scaling by ``1/n`` on the `ifft`.
+        #     For ``norm="ortho"``, both directions are scaled by ``1/sqrt(n)``.
+        # overwrite_x : bool, optional
+        #     If True, the contents of `x` can be destroyed; the default is False.
+        #     See the notes below for more details.
+        # workers : int, optional
+        #     Maximum number of workers to use for parallel computation. If negative,
+        #     the value wraps around from ``os.cpu_count()``. See below for more
+        #     details.
         # Returns
         # -------
         # out : complex ndarray
@@ -60,4 +88,9 @@ class Transformator(Extractor):
         #     if `axes` is larger than the last axis of `x`.
         
         #fields 
-        self.data
+        self.fdata = fft(x = self.data[:,channel], workers= -1) #complex value, __len__() = 300
+
+        def plot(self):
+            f = np.arange(0,300, self.samplesPerSecond)
+            plt.plot(f, self.fdata)
+            
