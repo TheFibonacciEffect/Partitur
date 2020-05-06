de9f7ec3c275fd5bdeb0ea869fb26fff6f90509a
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-05-06 20:49:25 +0200

    chunk data

--- demo.py
+++ demo.py
@@ -11,5 +11,5 @@ transform = Transformator(r"ressources\thegodfather.wav")
 
 #transform.plot()
 plt.plot(*transform.transform(sample_beginning=0, sample_end=1, frequency_beginning = 0, frequency_end =  4000, slicing=1))
-#plt.plot(*transform.findextrema(), 'x')
+plt.plot(*transform.findextrema(), 'x')
 plt.show()
