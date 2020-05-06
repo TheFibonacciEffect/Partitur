a48c1a2281b7d84c568c4b69dfb4b07c51f81765
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-05-06 22:08:16 +0200

    add further samples

--- demo.py
+++ demo.py
@@ -1,15 +1,15 @@
 from main import Extractor, Transformator
 
-extract = Extractor(r"ressources\thegodfather.wav")
+#extract = Extractor(r"ressources\Piano A.wav")
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 #extract.plot(sample_beginning= 0,sample_end=1)
 
-transform = Transformator(r"ressources\thegodfather.wav")
+transform = Transformator(r"ressources\B flat Trupet.wav") #ressources\Piano A.wav
 
 #transform.plot()
-plt.plot(*transform.transform(sample_beginning=0, sample_end=-1, frequency_beginning = 0, frequency_end =  4000, slicing=1, chunks = 100))
-plt.plot(*transform.findextrema(), 'x')
+plt.plot(*transform.transform(sample_beginning=1, sample_end=-1, frequency_beginning = 0, frequency_end =  4000, slicing=1, chunks = 1))
+#plt.plot(*transform.findextrema(), 'x')
 plt.show()
