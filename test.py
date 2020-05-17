69f6dc9f507a71ff3da752d589c682fa90f6cb06
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-05-17 10:02:52 +0200

    new test.py

--- test.py
+++ test.py
@@ -3,20 +3,11 @@ from main import Extractor, Transformator, Translator
 import matplotlib.pyplot as plt
 import numpy as np
 
-from scipy.io import wavfile
+translator = Translator(r"ressources\Piano A.wav")
 
-transform = Transformator(r"ressources\B flat Trupet.wav") #ressources\Piano A.wav
+plt.plot(*translator.transform(sampleBeginning = 10, sampleEnd = 12),"-")
+plt.plot(*translator.findMainFrequencies(2, sampleBeginning = 10, sampleEnd = 12),"x")
 
-plt.plot(*transform.transform(sample_beginning=1, sample_end=2, frequency_beginning = 0, frequency_end =  500, slicing=1, chunks = 10))
-plt.plot(*transform.findextrema(), 'x')
 plt.show()
-
-# plt.plot(*transform.transform(sample_beginning=1, sample_end=1, frequency_beginning = 0, frequency_end =  4000, slicing=1, chunks = 10))
-# plt.plot(*transform.findextrema(), 'x')
-
-translator = Translator(r"ressources\B flat Trupet.wav") #ressources\60 Hz Test Tone-GqwFimG3X3w.wav
-
-plt.plot(*translator.transform(sample_beginning=1, sample_end=2, frequency_beginning = 0, frequency_end =  500, slicing=1, chunks = 10))
-#plt.plot(*translator.findextrema(), 'x')
-plt.plot(*translator.findMainFrequencies(5), 'x')
+plt.plot(*translator.findMainFrequencies(2, sampleBeginningg=1, sampleEnd=2, frequency_beginning = 0, frequency_end =  500, slicing=1, chunks = 1), 'x')
 plt.show()
