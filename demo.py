4fc9c0ff68dc9fb9b1fe7b07393251f937423faf
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-05-20 22:09:54 +0200

    switch to camelCase

--- demo.py
+++ demo.py
@@ -11,13 +11,13 @@ extract.plot(sampleBeginning= 1,sampleEnd=2)
 transform = Transformator(r"ressources\Piano A.wav")
 
 #transform.plot()
-plt.plot(*transform.transform(sampleBeginning=1, sampleEnd=2, frequencyBeginning = 0, frequency_end =  500))
+plt.plot(*transform.transform(sampleBeginning=1, sampleEnd=2, frequencyBeginning = 0, frequencyEnd =  500))
 plt.plot(*transform.findextrema(), 'x')
 plt.show()
 
 translator = Translator(r"ressources\Piano A.wav") #ressources\60 Hz Test Tone-GqwFimG3X3w.wav
 
 plt.show()
-plt.plot(*translator.transform(sampleBeginning=1, sampleEnd=2, frequencyBeginning = 0, frequency_end =  500))
+plt.plot(*translator.transform(sampleBeginning=1, sampleEnd=2, frequencyBeginning = 0, frequencyEnd =  500))
 plt.plot(*translator.findMainFrequencies(5), 'x')
 plt.show()
