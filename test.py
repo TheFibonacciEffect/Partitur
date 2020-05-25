5964341eed970594721b4daf5f8599a23083c945
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-05-25 12:39:47 +0200

    add piano frequency range and optional log scale

--- test.py
+++ test.py
@@ -9,13 +9,14 @@ sampleEnd = 12
 chanel = 0
 sampleBeginning = 0
 sampleEnd = 2
-frequencyBeginning = 0
-frequencyEnd=1000
+#piano frequency range
+frequencyBeginning = 27.5
+frequencyEnd= 4186
 distance = 5
-number = 2  #number of the notes to detect
+number = 3  #number of the notes to detect
 fStartingNote = 440
 
-m = Main(r"ressources\Piano A.wav")
+m = Main(r"ressources\Piano A.wav") #ressources\B flat Trupet.wav
 m.main(chanel, sampleBeginning, sampleEnd, frequencyBeginning, frequencyEnd, distance, number, fStartingNote)
 
 plt.plot(m.xvalues, m.values)
@@ -25,4 +26,5 @@ plt.show()
 plt.plot(*m.fvalues_xy)
 plt.plot(*m.extrema, "b.")
 plt.plot(*m.mainFrequencies, "o")
+#plt.xscale("log")
 plt.show()
