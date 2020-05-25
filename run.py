95bd2a6549d5070d4d67dc81bfcdeee0285decb7
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-05-25 12:55:06 +0200

    update comments

--- run.py
+++ run.py
@@ -7,9 +7,10 @@ if __name__ == '__main__':
         m = Main(r"ressources\Piano A.wav")
 
         splitLengthinSeconds = 3
+        # multiprocessing
         song = m.split(splitLengthinSeconds, sampleBeginning = 0, sampleEnd = -1)
 
         notes = list(executor.map(m.thread, song))
 
-        #every list in the list is a triad [[0,12]] for example is an " a' " and an " a'' "
+        # every list in the list is a triad or more notes played at the same time [[0,12]] for example is an " a' " and an " a'' "
         print(notes)
