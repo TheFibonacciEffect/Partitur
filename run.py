6183960f1aeb174e96eed0eb9d3a35cd075d735e
Cthulhushauskatze <58665956+Cthulhushauskatze@users.noreply.github.com>
2020-05-29 13:46:54 +0200

    make removePartialRepetitions explicit in run.py

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         notes = list(executor.map(lambda data: m.thread(data, threshhold, numberOfNotes), song))
 
         # every list in the list is a triad or more notes played at the same time [[0,12]] for example is an " a' " and an " a'' "
-        notes = m.removeRepetitions(notes)
+        notes = m.removeRepetitions(notes, removePartialRepetitions=True)
         notes = m.noteNames(notes)
         print(len(notes))
         print(notes)
